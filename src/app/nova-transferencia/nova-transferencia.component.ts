d3cd5590cc0504bbcb15f633fb07db0b09b5979b
washingtoncrisostomo <washington.parra@gmail.com>
2022-05-30 23:55:40 -0300

    eventEmitter (propagando dados entre componentes))

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-nova-transferencia',
@@ -7,12 +7,20 @@ import { Component } from '@angular/core';
 })
 export class NovaTransferenciaComponent {
 
+  @Output() aoTransferir = new EventEmitter <any> ();
+
   valor: number;
   destino: number;
 
   transferir() {
     console.log('Solicitada nova transferência');
-    console.log('Valor: ', this.valor)
-    console.log('Destino: ', this.destino)
+    const valorEmitir = { valor: this.valor, destino: this.destino }
+    this.aoTransferir.emit(valorEmitir);
+    this.limparCampos();
+  }
+
+  limparCampos(){
+    this.valor = 0;
+    this.destino = 0;
   }
 }
