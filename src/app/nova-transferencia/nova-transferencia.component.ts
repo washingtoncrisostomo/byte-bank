6c70d1dd3c1b5948ffeea6f230341b652fe44a42
washingtoncrisostomo <washington.parra@gmail.com>
2022-06-20 21:08:03 -0300

    *ngIf Else e injecao de dependencia

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -13,7 +13,6 @@ export class NovaTransferenciaComponent {
   destino: number;
 
   transferir() {
-    console.log('Solicitada nova transferência');
     const valorEmitir = { valor: this.valor, destino: this.destino }
     this.aoTransferir.emit(valorEmitir);
     this.limparCampos();
